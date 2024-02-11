3ef59fe3aee9a782fa02f931ea3f8cae917e0a28
isretenovic <ivan.sretenovic@endava.com>
2024-02-11 21:46:43 +0100

    Initial commit

---
+++ Car Sharing/task/src/carsharing/Car.java
@@ -0,0 +1,30 @@
+package carsharing;
+
+public class Car {
+    private final int id;
+    private final String name;
+    private final int companyId;
+
+    Car(int id, String name, int companyId) {
+        this.id = id;
+        this.name = name;
+        this.companyId = companyId;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
